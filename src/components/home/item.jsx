9870a3cfcf6af6ffa9c724afa133bd2086e38377
osi-ic <osiic.offcl@gmail.com>
2023-04-29 00:12:47 +0700

    tolong lah bisa di deploy

--- src/components/home/item.jsx
+++
@@ -1,9 +0,0 @@
-export default function Item({ item: { title, content, price }, index }) {
-  return (
-    <article key={index} className="border p-4 rounded-md">
-      <h3 className="font-medium text-slate-800 text-lg">{title}</h3>
-      <h2 className="font-semibold text-2xl">{price}</h2>
-      <p className="font-normal text-slate-500">{content}</p>
-    </article>
-  );
-}
